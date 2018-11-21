b7eddb93472189e69bc2bc1bc8550187b4ad7024
Reza Khammary <r.khammary@gmail.com>
2018-11-21 18:00:20 +0330

    Updates

---
+++ precache-manifest.52597c31f535b1108192887156d472d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74ebc189917fe0335236",
+    "url": "/maktab-resume/static/js/runtime~main.74ebc189.js"
+  },
+  {
+    "revision": "0d4766ca77c59b15bcfa",
+    "url": "/maktab-resume/static/js/main.0d4766ca.chunk.js"
+  },
+  {
+    "revision": "6c7dfba206a4884959ea",
+    "url": "/maktab-resume/static/js/1.6c7dfba2.chunk.js"
+  },
+  {
+    "revision": "0d4766ca77c59b15bcfa",
+    "url": "/maktab-resume/static/css/main.321cea64.chunk.css"
+  },
+  {
+    "revision": "6e56672017e58ac63947de696fa19c58",
+    "url": "/maktab-resume/index.html"
+  }
+];
